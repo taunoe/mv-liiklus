ba47563d32cb1d5eb3889bad153ad9c2b1e8baa4
Tauno Erik <sedumacre@gmail.com>
2023-05-03 18:40:37 +0300

    models dir

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 File:    main.py
 Author:  Tauno Erik
 Started: 01.05.2023
-Edited:  01.05.2023
+Edited:  02.05.2023
 """
 
 import argparse
@@ -12,6 +12,8 @@ from ultralytics import YOLO  # pip install ultralytics
 import cv2
 import math
 import cvzone
+import os
+import mimetypes
 from datetime import datetime
 from liiklus_const import *
 
@@ -20,36 +22,51 @@ APPNAME = "Liiklus"
 LANG = EST # or ENG
 MODEL_FILE = './models/yolov8l.pt'
 CONF_TH = 0.6
+DATA_DIR = '/data/'
 WIDTH = 1280
 HEIGHT = 720
 
-is_active = True # while loop
+is_save_video = False
+is_active = True      # Main while loop
 
-# All model class names
-class_names = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-              ]
 
-detect_names = [ "person", "bicycle", "car",
-                 "motorbike", "bus", "truck",
-                 "dog", "horse", "cat",
-                 "bird"]
+def is_video(file):
+  '''
+  Returns: True or False
+  '''
+  if mimetypes.guess_type(file)[0].startswith('video'):
+    return True
+  return False
 
-colors = [(199,55,255), (56,56,255), (134,219,61),
-          (168,153,44),(151,157,255), (52,147,26),
-          (31,112,255), (29,178,255), (49,210,207),
-          (10,249,72), (23,204,146)]
 
-counter_line_l = [0, 0, 0, 0]
-counter_line_r = [0, 0, 0, 0]
+def process_file_input(filename):
+  """
+  Param video filename. Videos in DATA_DIR!
+  Returns full path to video
+  """
+  filepath = os.getcwd() + DATA_DIR + filename  # Absolute path to input
+  print(filepath)
+
+  if os.path.isfile(filepath):
+    if not is_video(filepath):
+      print("Not a video!")
+    else:
+      return filepath
+  else:
+    print("{} - is not a file!".format(filename))
+
+def calculate_count_line(cap, name):
+  w = cap.get(3)
+  h = cap.get(4)
+  y1 = 50
+  y2 = int(h-50)
+  if name == 'right':
+    x1 = int((w/2)+50)
+    x2 = int((w/2)+50)
+  if name == 'left':
+    x1 = int((w/2)-50)
+    x2 = int((w/2)-50)
+  return [x1, y1, x2, y2]
 
 
 def main(argv):
@@ -58,50 +75,64 @@ def main(argv):
   is_webcam = True
 
   # Optional arguments:
-  parser.add_argument("-v","--video", type=str, help="Input video file.")
+  parser.add_argument("-v","--video", type=str, help="Input video file name.")
 
   args = parser.parse_args()
 
   if args.video:
      is_webcam = False
+     video_name = args.video
+     video_path = process_file_input(video_name)
+     print(video_path)
 
   model = YOLO(MODEL_FILE)
   model.fuse()  # Mida teeb?
 
-  cap = cv2.VideoCapture(0)  # webcam
-  cap.set(3, WIDTH)
-  cap.set(4, HEIGHT)
-
-  if not cap.isOpened():
-    raise IOError("Cannot open webcam")
+  if is_webcam:
+    # Open webcamera
+    cap = cv2.VideoCapture(0)  # webcam
+    #cap.set(3, WIDTH)
+    #cap.set(4, HEIGHT)
+    if not cap.isOpened():
+      raise IOError("Cannot open webcam!")
+  else:
+    # Open video fail
+    cap = cv2.VideoCapture(video_path)
+    if not cap.isOpened():
+      raise IOError("Cannot open video!")
+  
+  # Loendus joon
+  counter_l = calculate_count_line(cap, 'left')
+  counter_r = calculate_count_line(cap, 'right')
 
   while is_active:
     is_cap, frame = cap.read()
-
     if is_cap:
       results = model(frame, stream=True)
-
       for r in results:
         boxes = r.boxes
-
         for box in boxes:
           x1, y1, x2, y2 = box.xyxy[0]
           x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
           w, h = x2-x1, y2-y1
 
-          conf = math.ceil((box.conf[0]*100))/100 # round
+          #conf = math.ceil((box.conf[0]*100))/100 # round
           class_id = int(box.cls[0])
           class_name = model.names[class_id]
 
-          if class_name in detect_names:
-            color_index = detect_names.index(class_name)
-            cv2.rectangle(frame, (x1,y1), (x2,y2), colors[color_index], 3)
+          if class_name in detect_names: 
+            index = detect_names.index(class_name)
+            cv2.rectangle(frame, (x1,y1), (x2,y2), colors[index], 3)
             cvzone.putTextRect(frame, 
-                               f'{class_name}:{conf}',
+                               f'{lang_txt[LANG][index]}',
                                (max(0,x1)+8, max(30, y1)-8),
-                               colorB=colors[color_index]
+                               colorB=colors[index],
+                               colorR=colors[index],
+                               colorT=(10,10,10)
                                )
-
+      # Draw counting lines
+      cv2.line(frame, (counter_l[0], counter_l[1]), (counter_l[2], counter_l[3]), colors[10], 3)
+      cv2.line(frame, (counter_r[0], counter_r[1]), (counter_r[2], counter_r[3]), colors[10], 3)
     else:
        print("Bad cap")
 
---
+++ models/README.md
@@ -0,0 +1,3 @@
+# Models
+
+[YOLO Releases](https://github.com/ultralytics/assets/releases/)
